639264a25da46fbb32da9ccf505d6935a056f91a
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-03-08 02:08:38 -0500

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -95,12 +95,24 @@ function App() {
     })
   }
 
+  const image_input = document.querySelector("#image_input");
+  var uploaded_image;
+
+  image_input.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      uploaded_image = reader.result;
+      document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+    });
+    reader.readAsDataURL(this.files[0]);
+  });
+
   return (
     <div className="App">
       <div className="Input">
         <h1>Input</h1>
         <form onSubmit={handleSubmit}>
-          <input type="file" accept=".png" onChange={handleChange} />
+          <input type="file" id="image_input" accept=".png" onChange={handleChange} />
           <button type="submit" disabled={buttonDisable}>{buttonText}</button>
           <div id="display_image"></div>
         </form>
