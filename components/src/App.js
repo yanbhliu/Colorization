8e712238881f61ee388c753892d6c585bcdb0aa0
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-03-07 20:18:58 -0500

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -65,7 +65,7 @@ function App() {
 
     // make POST request
     console.log('making POST request...');
-    fetch('<api-url>', {
+    fetch('https://hprmi9pxe6.execute-api.us-east-1.amazonaws.com/prod/', {
       method: 'POST',
       headers: { "Content-Type": "application/json", "Accept": "text/plain" },
       body: JSON.stringify({ "image": inputFileData })
