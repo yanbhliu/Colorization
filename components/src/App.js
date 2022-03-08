d20a661056d32d4397ad724bc671a96abd590ca4
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-03-08 02:21:44 -0500

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -95,17 +95,17 @@ function App() {
     })
   }
 
-  const image_input = document.querySelector("#image_input");
-  var uploaded_image;
+//   const image_input = document.querySelector("#image_input");
+//   var uploaded_image;
 
-  image_input.addEventListener('change', function() {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      uploaded_image = reader.result;
-      document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
-    });
-    reader.readAsDataURL(this.files[0]);
-  });
+//   image_input.addEventListener('change', function() {
+//     const reader = new FileReader();
+//     reader.addEventListener('load', () => {
+//       uploaded_image = reader.result;
+//       document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+//     });
+//     reader.readAsDataURL(this.files[0]);
+//   });
 
   return (
     <div className="App">
