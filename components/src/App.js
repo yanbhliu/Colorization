d20142a5ea1cf58d29de87f01751f923802a896d
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-03-30 13:36:50 -0400

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -140,7 +140,7 @@ function App() {
       </div>
     <h2>Report</h2>
     <center>
-    <iframe src = "https://drive.google.com/file/d/1iEeYnR8MjwQs4ECgiDeNlMZtONfkLRdM/view?usp=sharing" width="640" height = "480"></iframe>
+    <embed src="https://github.com/yanbhliu/Colorization/blob/main/components/src/Colorization.pdf" type="application/pdf" width="640" height = "480" />
     </center>
     </div>
   );
