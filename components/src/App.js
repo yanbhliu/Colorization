6c7e6a78cf249db29135e7f8328ebeed93ae501a
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-03-31 01:45:56 -0400

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -158,11 +158,14 @@ function App() {
 
 
   return (
+    <div className = "Other">
+         <h1>Grayscale Image Colorization Demo</h1>
+         <h3>Yanbaihui Liu</h3>
+         <img src={demo} alt="demo" width="304" height="152"/>;
+    </div>
+
     <div className="App">
-     <h1>Grayscale Image Colorization Demo</h1>
-      <h3>Yanbaihui Liu</h3>
-      <img src={demo} alt="demo" width="304" height="152"/>;
-      <div className="Grayscale Input">
+      <div className="Input">
         <h2>Input</h2>
         <p>choose from dropdown manual or click "Upload File" to upload an grayscale image, then click submit</p>
         <label htmlFor="demo-dropdown">Demo: </label>
@@ -190,12 +193,14 @@ function App() {
      </center>
      </picture>
       </div>
+    </div>
 
+ <div className="Other">
     <h2>Report</h2>
     <center>
     <iframe src="https://drive.google.com/file/d/1iEeYnR8MjwQs4ECgiDeNlMZtONfkLRdM/preview" width="640" height="480" allow="autoplay"></iframe>
     </center>
-    </div>
+</div>
   );
 }
 
