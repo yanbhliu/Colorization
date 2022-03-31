1329ea91c7660256df9e59db25e31daee3b3b35a
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-03-31 00:47:51 -0400

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React from 'react';
-import demo from './demo.jpg';
 
-console.log(demo);
+// global variables to change where necessary
+const DROPDOWN_API_ENDPOINT = 'https://jgvnzlk00j.execute-api.us-east-1.amazonaws.com/prod/'; // GET/POST REST API 
+const ML_API_ENDPOINT = 'https://5uzxda7pe9.execute-api.us-east-1.amazonaws.com/prod/'; // POST REST API
+
 
 // atob is deprecated but this function converts base64string to text string
 const decodeFileBase64 = (base64String) => {
@@ -10,15 +12,38 @@ const decodeFileBase64 = (base64String) => {
   return "data:image/jpg;base64," + base64String
 };
 
+
 function App() {
   const [inputFileData, setInputFileData] = React.useState(''); // represented as bytes data (string)
   const [outputFileData, setOutputFileData] = React.useState(''); // represented as readable data (text string)
+  const [inputImage, setInputImage] = React.useState(''); // represented as bytes data (string)
   const [buttonDisable, setButtonDisable] = React.useState(true);
-  const [buttonText, setButtonText] = React.useState('Submit');
+  const [submitButtonText, setSubmitButtonText] = React.useState('Submit');
+  const [fileButtonText, setFileButtonText] = React.useState('Upload File');
+  const [demoDropdownFiles, setDemoDropdownFiles] = React.useState([]);
+  const [selectedDropdownFile, setSelectedDropdownFile] = React.useState('');
+
+  // make GET request to get demo files on load -- takes a second to load
+  React.useEffect(() => {
+    fetch(DROPDOWN_API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      // GET request error
+      if (data.statusCode === 400) {
+        console.log('Sorry! There was an error, the demo files are currently unavailable.')
+      }
+
+      // GET request success
+      else {
+        const s3BucketFiles = JSON.parse(data.body);
+        setDemoDropdownFiles(s3BucketFiles["s3Files"]);
+      }
+    });
+  }, [])
+
 
   // convert file to bytes data
   const convertFileToBytes = (inputFile) => {
-    console.log('converting file to bytes...');
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.readAsDataURL(inputFile); // reads file as bytes data
@@ -33,25 +58,31 @@ function App() {
     });
   }
 
+
   // handle file input
   const handleChange = async (event) => {
-    // Clear output text.
-    setOutputFileData("");
-
-    console.log('newly uploaded file');
     const inputFile = event.target.files[0];
-    console.log(inputFile);
+
+    // update file button text
+    setFileButtonText(inputFile.name);
 
     // convert file to bytes data
     const base64Data = await convertFileToBytes(inputFile);
+    setInputImage(base64Data);
     const base64DataArray = base64Data.split('base64,'); // need to get rid of 'data:image/png;base64,' at the beginning of encoded string
     const encodedString = base64DataArray[1];
     setInputFileData(encodedString);
-    console.log('file converted successfully');
 
     // enable submit button
     setButtonDisable(false);
+
+    // clear response results
+    setOutputFileData('');
+
+    // reset demo dropdown selection
+    setSelectedDropdownFile('');
   }
+
 
   // handle file submission
   const handleSubmit = (event) => {
@@ -59,19 +90,15 @@ function App() {
 
     // temporarily disable submit button
     setButtonDisable(true);
-    setButtonText('Loading Result');
+    setSubmitButtonText('Loading Result...');
 
     // make POST request
-    console.log('making POST request...');
-    fetch('https://5uzxda7pe9.execute-api.us-east-1.amazonaws.com/prod/', {
+    fetch(ML_API_ENDPOINT, {
       method: 'POST',
       headers: { "Content-Type": "application/json", "Accept": "text/plain" },
       body: JSON.stringify({ "image": inputFileData })
     }).then(response => response.json())
     .then(data => {
-      console.log('getting response...')
-      console.log(data);
-
       // POST request error
       if (data.statusCode === 400) {
         const outputErrorMessage = JSON.parse(data.errorMessage)['outputResultsData'];
@@ -86,48 +113,67 @@ function App() {
 
       // re-enable submit button
       setButtonDisable(false);
-      setButtonText('Submit');
-    })
-    .then(() => {
-      console.log('POST request success');
+      setSubmitButtonText('Submit');
     })
   }
 
-//   const image_input = document.querySelector("#image_input");
-//   var uploaded_image;
 
-//   image_input.addEventListener('change', function() {
-//     const reader = new FileReader();
-//     reader.addEventListener('load', () => {
-//       uploaded_image = reader.result;
-//       document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
-//     });
-//     reader.readAsDataURL(this.files[0]);
-//   });
+  // handle demo dropdown file selection
+  const handleDropdown = (event) => {
+    setSelectedDropdownFile(event.target.value);
+
+    // temporarily disable submit button
+    setButtonDisable(true);
+    setSubmitButtonText('Loading Demo File...');
+
+    // only make POST request on file selection
+    if (event.target.value) {
+      fetch(DROPDOWN_API_ENDPOINT, {
+        method: 'POST',
+        body: JSON.stringify({ "fileName": event.target.value })
+      }).then(response => response.json())
+      .then(data => {
+
+        // POST request error
+        if (data.statusCode === 400) {
+          console.log('Uh oh! There was an error retrieving the dropdown file from the S3 bucket.')
+        }
+
+        // POST request success
+        else {
+          const dropdownFileBytesData = JSON.parse(data.body)['bytesData'];
+          setInputFileData(dropdownFileBytesData);
+          setInputImage('data:image/png;base64,' + dropdownFileBytesData); // hacky way of setting image from bytes data - even works on .jpeg lol
+          setSubmitButtonText('Submit');
+          setButtonDisable(false);
+        }
+      });
+    }
+
+    else {
+      setInputFileData('');
+    }
+  }
+
 
   return (
     <div className="App">
-    <h1>Grayscale Image Colorization Demo</h1>
-    <h3>Yanbaihui Liu</h3>
-     <img src={demo} alt="demo" width="304" height="152"/>;
-      <div className="Grayscale Input">
-        <h2>Input</h2>
-        <p>click "Choose File" to upload an grayscale image</p>
+      <div className="Input">
+        <h1>Input</h1>
+        <label htmlFor="demo-dropdown">Demo: </label>
+        <select name="Select Image" id="demo-dropdown" value={selectedDropdownFile} onChange={handleDropdown}>
+            <option value="">-- Select Demo File --</option>
+            {demoDropdownFiles.map((file) => <option key={file} value={file}>{file}</option>)}
+        </select>
         <form onSubmit={handleSubmit}>
-          <input type="file" id="image_input" accept=".png" onChange={handleChange} />
-          <button type="submit" disabled={buttonDisable}>{buttonText}</button>
+          <label htmlFor="file-upload">{fileButtonText}</label>
+          <input type="file" id="file-upload" onChange={handleChange} />
+          <button type="submit" disabled={buttonDisable}>{submitButtonText}</button>
         </form>
-        <p></p>
+        <img src={inputImage} alt="" />
       </div>
-     <center><div id="display_image">
-       < img src={`data:;base64,${inputFileData}`} alt="waiting for input image (in .png or .jpg)" 
-          width="152" 
-          height="152"/>
-     </div>
-     </center>
-     
       <div className="Output">
-        <h2>Colorized Results</h2>
+        <h1>Results</h1>
         <picture>
         <center><div id="display_image">
        < img src={outputFileData} alt="waiting for result..." 
@@ -135,12 +181,8 @@ function App() {
           height="152"/>
      </div>
      </center>
-        </picture>
+     </picture>
       </div>
-    <h2>Report</h2>
-    <center>
-    <iframe src="https://drive.google.com/file/d/1iEeYnR8MjwQs4ECgiDeNlMZtONfkLRdM/preview" width="640" height="480" allow="autoplay"></iframe>
-    </center>
     </div>
   );
 }
